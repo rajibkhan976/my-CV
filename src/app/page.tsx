091bf80e7331b003559365b9ae0f2d5ae4eb8f8a
rajibkhan976 <rajibkhan976@gmail.com>
2025-01-16 11:31:47 +0800

    cv project created

---
+++ src/app/page.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button";
+import {
+	DialogActionTrigger,
+	DialogBody,
+	DialogCloseTrigger,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogRoot,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+
+export default function Home() {
+	return (
+		<DialogRoot>
+			<DialogTrigger asChild>
+				<Button
+					variant='outline'
+					size='sm'
+				>
+					Open Dialog
+				</Button>
+			</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Dialog Title</DialogTitle>
+				</DialogHeader>
+				<DialogBody>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</p>
+				</DialogBody>
+				<DialogFooter>
+					<DialogActionTrigger asChild>
+						<Button variant='outline'>Cancel</Button>
+					</DialogActionTrigger>
+					<Button>Save</Button>
+				</DialogFooter>
+				<DialogCloseTrigger />
+			</DialogContent>
+		</DialogRoot>
+	);
+}
