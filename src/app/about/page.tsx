f9e3bbffab43a044efffa2ab35db4f8ea890795d
rajibkhan976 <rajibkhan976@gmail.com>
2025-01-24 16:51:40 +0800

    about page completed, working on resume page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,36 +2,90 @@ import { List } from "@chakra-ui/react";
 
 const About = () => {
 	return (
-		<div className='f-full text-black p-4'>
-			<div className='text-black font-semibold text-2xl text-center my-4'>
-				About me
+		<div className='f-full text-black p-4 flex flex-col'>
+			<div className='text-black font-semibold text-2xl my-4'>About me</div>
+			<div className='w-full flex'>
+				<div className='w-5/12'>
+					<div className='text-black font-semibold text-lg'>
+						Communication Skills
+					</div>
+					<ul className='list-disc list-inside m-2'>
+						<li>
+							Ability to explain technical concepts to non-technical
+							stakeholders.
+						</li>
+						<li>Clear and concise written and verbal communication.</li>
+						<li>Active listening to understand requirements and feedback.</li>
+					</ul>
+					<div className='text-black font-semibold text-lg'>
+						Problem-Solving Skills
+					</div>
+					<ul className='list-disc list-inside m-2'>
+						<li>Analytical thinking to identify and resolve issues.</li>
+						<li>Creativity in designing innovative solutions.</li>
+						<li>Resilience to troubleshoot and debug complex problems.</li>
+					</ul>
+					<div className='text-black font-semibold text-lg'>
+						Teamwork and Collaboration
+					</div>
+					<ul className='list-disc list-inside m-2'>
+						<li>Prioritizing tasks to meet project deadlines.</li>
+						<li>Multitasking across different projects or modules.</li>
+						<li>Staying organized in fast-paced development environments.</li>
+					</ul>
+					<div className='text-black font-semibold text-lg'>
+						Time Management and Organization
+					</div>
+					<ul className='list-disc list-inside m-2'>
+						<li>Prioritizing tasks to meet project deadlines.</li>
+						<li>Multitasking across different projects or modules.</li>
+						<li>Staying organized in fast-paced development environments.</li>
+					</ul>
+				</div>
+				<div className='w-5/12'>
+					<div className='text-black font-semibold text-lg'>
+						Adaptability and Learning Agility
+					</div>
+					<ul className='list-disc list-inside m-2'>
+						<li>Openness to new tools, frameworks, and methodologies.</li>
+						<li>
+							Flexibility to adapt to changing project requirements or
+							technologies.
+						</li>
+						<li>Commitment to continuous improvement and upskilling.</li>
+					</ul>
+					<div className='text-black font-semibold text-lg'>
+						Attention to Detail
+					</div>
+					<ul className='list-disc list-inside m-2'>
+						<li>Ensuring precision in code and project requirements.</li>
+						<li>
+							Testing thoroughly to identify potential bugs or inefficiencies.
+						</li>
+					</ul>
+
+					<div className='text-black font-semibold text-lg'>
+						Empathy and User-Centric Thinking
+					</div>
+					<ul className='list-disc list-inside m-2'>
+						<li>
+							Understanding user’s needs to create intuitive software solutions.
+						</li>
+						<li>Considering accessibility and inclusivity in designs.</li>
+					</ul>
+
+					<div className='text-black font-semibold text-lg'>
+						Critical Thinking
+					</div>
+					<ul className='list-disc list-inside m-2'>
+						<li>
+							Evaluating the impact of decisions on system performance and
+							scalability.
+						</li>
+						<li>Making informed decisions based on data and analysis.</li>
+					</ul>
+				</div>
 			</div>
-			<div className='text-black font-semibold text-lg'>
-				Communication Skills
-			</div>
-			<ul className='list-disc list-inside m-2 ps-4'>
-				<li>
-					Ability to explain technical concepts to non-technical stakeholders.
-				</li>
-				<li>Clear and concise written and verbal communication.</li>
-				<li>Active listening to understand requirements and feedback.</li>
-			</ul>
-			<div className='text-black font-semibold text-lg'>
-				Problem-Solving Skills
-			</div>
-			<ul className='list-disc list-inside m-2 ps-4'>
-				<li>Analytical thinking to identify and resolve issues.</li>
-				<li>Creativity in designing innovative solutions.</li>
-				<li>Resilience to troubleshoot and debug complex problems.</li>
-			</ul>
-			<div className='text-black font-semibold text-lg ps-4'>
-				Teamwork and Collaboration
-			</div>
-			<ul className='list-disc list-inside m-2'>
-				<li>Prioritizing tasks to meet project deadlines.</li>
-				<li>Multitasking across different projects or modules.</li>
-				<li>Staying organized in fast-paced development environments.</li>
-			</ul>
 		</div>
 	);
 };
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -28,6 +28,65 @@ const Resume = () => {
 					cutting-edge projects while enhancing organizational goals.
 				</div>
 			</div>
+			<div className='w-full px-8 mt-4'>
+				<div className='text-black font-bold text-xl mb-2 uppercase border-b-2 border-gray-950'>
+					KEY SKILLS
+				</div>
+				<ul className='list-disc list-inside m-2 text-black'>
+					<li className='py-1'>
+						<span className='font-semibold'>Frontend Development:&nbsp;</span>
+						JavaScript (ES6+), React, Redux, Next.js, TypeScript, Angular,
+						Vue.js, HTML5, CSS3, Tailwind CSS, Bootstrap, SCSS, GSAP
+					</li>
+					<li className='py-1'>
+						<span className='font-semibold'>Backend Development:&nbsp;</span>
+						Node.js, Express, NestJS, Python, Django, Flask, PHP, Laravel,
+						ASP.NET MVC, ASP.NET Web API, C#, Java, Spring Boot, Maven
+					</li>
+					<li className='py-1'>
+						<span className='font-semibold'>Databases:&nbsp;</span>
+						SQL, PostgreSQL, Microsoft SQL Server, MongoDB, MySQL, GraphQL
+					</li>
+					<li className='py-1'>
+						<span className='font-semibold'>DevOps & Tools:&nbsp;</span>
+						Docker, AWS (EC2, S3), Git (GitHub, GitLab, Bitbucket), Jira, npm,
+						Webpack, Babel, Azure
+					</li>
+					<li className='py-1'>
+						<span className='font-semibold'>Testing & Frameworks:&nbsp;</span>
+						Jest, Mocha, Cypress, Storybook, Microservices, RabbitMQ, Redis,
+						Stripe API, OOP, REST APIs, Agile, Scrum
+					</li>
+				</ul>
+			</div>
+			<div className='w-full px-8'>
+				<div className='text-black font-bold text-xl mb-2 uppercase border-b-2 border-gray-950'>
+					Education
+				</div>
+				<div className='text-black text-justify flex justify-between py-1'>
+					<div className='flex flex-col'>
+						<div className='font-semibold'>Master of Information Systems </div>
+						<div className=''>
+							Linnaeus University, Växjö, Sweden (Completed 90 credits)
+						</div>
+					</div>
+					<div className='font-semibold'>2019</div>
+				</div>
+				<div className='text-black text-justify flex justify-between py-1'>
+					<div className='flex flex-col'>
+						<div className='font-semibold'>Master of Information Science</div>
+						<div className=''>University of Dhaka, Dhaka, Bangladesh</div>
+					</div>
+					<div className='font-semibold'>2014</div>
+				</div>
+				<div className='text-black text-justify flex justify-between py-1'>
+					<div className='flex flex-col'>
+						<div className='font-semibold'>Bachelor of Information Science</div>
+						<div className=''>University of Dhaka, Dhaka, Bangladesh</div>
+					</div>
+					<div className='font-semibold'>2013</div>
+				</div>
+			</div>
 		</div>
 	);
 };
