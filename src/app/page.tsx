7f646f6c073a06da527f53d893e2003cb6295bfc
rajibkhan976 <rajibkhan976@gmail.com>
2025-01-24 08:44:52 +0800

    card responsive issue is fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 import { Image } from "@chakra-ui/react";
 
 const Home = () => (
-	<div className='w-full p-4 flex justify-center h-[500px]'>
-		<div className='w-7/12 h-5/6 flex ring-1 ring-gray-400 rounded-sm mt-12'>
+	<div className='w-full p-4 flex justify-center'>
+		<div className='w-11/12 md:flex md:flex-row ring-1 ring-gray-400 rounded-sm mt-12 sm:flex-col sm:justify-start'>
 			<Image
-				className='rounded-sm'
+				className='md:w-3/12 md:h-[500px] h-auto w-full rounded-sm'
 				src='/Resume-Home.jpeg'
 				alt=''
 			/>
-			<div className='h-5/6 flex flex-col justify-between'>
+			<div className='md:w-8/12 w-full h-11/12 flex flex-col justify-start md:ps-4 pe-2'>
 				<div className='text-black text-lg font-semibold ps-4 mt-4'>
 					Rajib Hossain Khan
 				</div>
-				<div className='text-gray-600 ps-4 mt-4'>
+				<div className='text-gray-600 ps-4 mt-4 text-justify'>
 					I am an aspiring software developer with a strong foundation in
 					programming, problem-solving, and system design, seeking a dynamic
 					role in a forward-thinking organization. Passionate about leveraging
