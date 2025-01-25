e2d2019dab6b431919df7fc1b42e3d34a4a58291
rajibkhan976 <rajibkhan976@gmail.com>
2025-01-25 10:47:48 +0800

    contact card design initially completed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,59 +1,7 @@
+import ContactCard from "@/components/ContactCard";
+
 const Contact = () => {
-	return (
-		<div className='text-black p-4 w-full h-screen flex justify-center items-center'>
-			<div className='w-8/12 flex flex-col bg-white shadow-lg p-4'>
-				<div className='flex justify-between mb-4'>
-					<label
-						className='me-4'
-						htmlFor='subject'
-					>
-						Subject:
-					</label>
-					<input
-						id='subject'
-						type='text'
-						width={"400px"}
-						className='bg-white ring-1 ring-gray-700 rounded-sm py-1 px-2 w-9/12'
-						name='subject'
-						placeholder='Subject'
-						required
-					/>
-				</div>
-				<div className='flex justify-between mb-4'>
-					<label
-						className='me-4'
-						htmlFor='email'
-					>
-						Email
-					</label>
-					<input
-						id='email'
-						type='email'
-						className='bg-white ring-1 ring-gray-700 rounded-sm py-1 px-2 w-9/12'
-						name='email'
-						placeholder='Email'
-						required
-					/>
-				</div>
-				<div className='flex justify-between mb-4'>
-					<label
-						className='me-4'
-						htmlFor='message'
-					>
-						Message
-					</label>
-					<textarea
-						id='message'
-						className='bg-white ring-1 ring-gray-700 rounded-sm py-1 px-2 w-9/12'
-						name='message'
-						rows={3}
-						placeholder='Message'
-						required
-					></textarea>
-				</div>
-			</div>
-		</div>
-	);
+	return <ContactCard />;
 };
 
 export default Contact;
