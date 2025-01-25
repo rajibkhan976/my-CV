1cd56b5894e5afe4f85bba8bd968ce1670891b74
rajibkhan976 <rajibkhan976@gmail.com>
2025-01-25 08:12:09 +0800

    working on contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,43 +1,57 @@
-import { Button, Fieldset, Input, Stack, Textarea } from "@chakra-ui/react";
-import { Field } from "@/components/ui/field";
-
 const Contact = () => {
 	return (
-		<div className='text-black p-4'>
-			<Fieldset.Root
-				size='lg'
-				maxW='md'
-			>
-				<Stack>
-					<Fieldset.Legend className='text-black'>
-						Contact details
-					</Fieldset.Legend>
-					<Fieldset.HelperText>
-						Please provide your contact details below.
-					</Fieldset.HelperText>
-				</Stack>
-
-				<Fieldset.Content>
-					<Field label='Subject'>
-						<Input name='subject' />
-					</Field>
-
-					<Field label='Email address'>
-						<Input
-							name='email'
-							type='email'
-						/>
-					</Field>
-
-					<Field label='Message'>
-						<Textarea placeholder='Message' />
-					</Field>
-				</Fieldset.Content>
-
-				<button className='bg-green-600 text-white w-20 h-8 rounded-md font-semibold'>
-					Submit
-				</button>
-			</Fieldset.Root>
+		<div className='text-black p-4 w-full h-screen flex justify-center items-center'>
+			<div className='w-8/12 flex flex-col bg-white shadow-lg p-4'>
+				<div className='flex justify-between mb-4'>
+					<label
+						className='me-4'
+						htmlFor='subject'
+					>
+						Subject:
+					</label>
+					<input
+						id='subject'
+						type='text'
+						width={"400px"}
+						className='bg-white ring-1 ring-gray-700 rounded-sm py-1 px-2 w-9/12'
+						name='subject'
+						placeholder='Subject'
+						required
+					/>
+				</div>
+				<div className='flex justify-between mb-4'>
+					<label
+						className='me-4'
+						htmlFor='email'
+					>
+						Email
+					</label>
+					<input
+						id='email'
+						type='email'
+						className='bg-white ring-1 ring-gray-700 rounded-sm py-1 px-2 w-9/12'
+						name='email'
+						placeholder='Email'
+						required
+					/>
+				</div>
+				<div className='flex justify-between mb-4'>
+					<label
+						className='me-4'
+						htmlFor='message'
+					>
+						Message
+					</label>
+					<textarea
+						id='message'
+						className='bg-white ring-1 ring-gray-700 rounded-sm py-1 px-2 w-9/12'
+						name='message'
+						rows={3}
+						placeholder='Message'
+						required
+					></textarea>
+				</div>
+			</div>
 		</div>
 	);
 };
