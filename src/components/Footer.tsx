b162211384138d337ba0cac35af08258ae52201f
rajibkhan976 <rajibkhan976@gmail.com>
2025-01-28 21:52:46 +0800

    copyright icon size increased

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
 		<div className='bg-black w-full h-16 flex md:flex-row flex-col items-center'>
 			<div className='flex items-center text-white font-semibold md:w-5/12 w-full md:my-0 my-2 ms-2'>
 				{/* <RiCopyrightFill className='ms-4 text-2xl' /> */}
-				&copy;
+				<span className='text-2xl'>&copy;</span>
 				<span className='text-sm align-middle'>
 					&nbsp;{date.getFullYear()} All rights reserved to Rajib Hossain Khan
 				</span>
