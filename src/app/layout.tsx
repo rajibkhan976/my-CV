608ef0cb0cf9ba37d95e23a6684830903771917c
rajibkhan976 <rajibkhan976@gmail.com>
2025-01-29 20:19:18 +0800

    page height fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
 			suppressHydrationWarning
 		>
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen`}
 			>
 				<Provider>
 					<div className='flex md:flex-row flex-col justify-between p-4'>
